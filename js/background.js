c211d4e817460d1c0ec739724985e80b4d10de65
iyaozhen <yaozhen@baidu.com>
2014-12-09 14:56:14 +0800

    add: background.js

---
+++ js/background.js
@@ -0,0 +1,9 @@
+/**
+ * Created by yaozhen on 2014/12/9.
+ */
+function baidu(info, tab){
+    var text=encodeURIComponent(info.selectionText);
+    var url = "http://www.baidu.com/s?tn=se_chromelist&wd=" + text;
+    window.open(url);
+}
+chrome.contextMenus.create({"title": "创建Todo","contexts":["selection"],"onclick":baidu});
