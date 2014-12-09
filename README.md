703c9f43686a6c7fde6b29e4630926ed4fa6a1b4
iyaozhen <yaozhen@baidu.com>
2014-12-09 21:50:49 +0800

    add: logo

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ Hi-Todo-Extension
 =================
 
 百度Hi Todo浏览器扩展
+
+Hackathon参赛代码示例
--- js/background.js
+++ js/background.js
@@ -2,7 +2,7 @@
  * Created by yaozhen on 2014/12/9.
  */
 function baidu(info, tab){
-    var text=encodeURIComponent(info.selectionText);
+    var text = encodeURIComponent(info.selectionText);
     var url = "http://www.baidu.com/s?tn=se_chromelist&wd=" + text;
     window.open(url);
 }
