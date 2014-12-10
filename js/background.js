46bd0e2b62377c6ee34ce5e14cc33cb267fa20b7
iyaozhen <yaozhen@baidu.com>
2014-12-10 22:05:35 +0800

    add: QRC

--- js/background.js
+++ js/background.js
@@ -6,4 +6,11 @@ function baidu(info, tab){
     var url = "http://www.baidu.com/s?tn=se_chromelist&wd=" + text;
     window.open(url);
 }
-chrome.contextMenus.create({"title": "创建Todo","contexts":["selection"],"onclick":baidu});
+function create_qrc(){
+    chrome.extension.sendRequest("gkghoadjnoembhnkpjkomhgngdnllngg", "createQRC", function(response) {
+        console.log(response);
+    });
+}
+//chrome.contextMenus.create({"title": "创建Todo","contexts":["selection"],"onclick":baidu});
+chrome.contextMenus.create({"title": "网页二维码", "onclick": create_qrc});
+create_qrc();
--- js/content-scripts.js
+++ js/content-scripts.js
@@ -1,4 +1,19 @@
 /**
  * Created by yaozhen on 2014/12/8.
  */
-console.log("test");
+chrome.extension.onRequest.addListener(function(message, sender, sendResponse) {
+    console.log("ok");
+    sendResponse('Create QRC OK.');
+    //if(message == 'createQRC'){
+    //    var url = encodeURI(location.href);
+    //    var qrcImgUrl = "http://qr.liantu.com/api.php?text=" + url;
+    //    var qrcImg = '<img style="width: 200px; height: 200px; position: fixed; top: 0; left: 0" src="' + qrcImgUrl + '" alt=""/>';
+    //    $("html").append(qrcImg);
+    //    sendResponse('Create QRC OK.');
+    //}
+    //else{
+    //    sendResponse('Create QRC fail.');
+    //}
+});
+
+http://stackoverflow.com/questions/14452777/is-that-possible-calling-content-script-method-by-context-menu-item-in-chrome-ex
