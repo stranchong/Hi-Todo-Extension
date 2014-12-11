6dc5f9f1726954e51819029569283fb15491335b
iyaozhen <yaozhen@baidu.com>
2014-12-11 18:29:28 +0800

    add: hi share

--- js/background.js
+++ js/background.js
@@ -1,8 +1,36 @@
 /**
  * 后台程序
  */
+// 创建Todo
+function create_todo(){
+
+}
+// 创建Todo（有选择文本）
+function create_todo_select(){
+    var text = encodeURIComponent(info.selectionText);
+}
+function share_to_hi(info, tab){
+    var pageUrl = encodeURIComponent(tab.url);
+    var title = encodeURI(tab.title);
+    var imgUrl;
+    var description;
+    var apiUrl;
+    // 异步回调请求数据
+    chrome.tabs.sendMessage(tab.id, "getPageImg", function(response) {
+        imgUrl = encodeURI(response);
+        chrome.tabs.sendMessage(tab.id, "getPageDes", function(response) {
+            description = encodeURI(response);
+            apiUrl = "http://open.im.baidu.com/share/html/share.html?resourceUrl=" + pageUrl + "&pic=" + imgUrl + "&title=" + title + "&description=" + description + "&tip=" + encodeURI("来自百度Hi Todo浏览器扩展") + "&charset=UTF-8&rt=1";
+            window.open(apiUrl);
+        });
+    });
+}
+// 创建二维码
 function create_qrc(info, tab){
     chrome.tabs.sendMessage(tab.id, "createQRC");
 }
 // 右键菜单
+chrome.contextMenus.create({"title": "创建Todo，关于「%s」", "contexts":["selection"], "onclick": create_todo_select});
+chrome.contextMenus.create({"title": "创建Todo", "onclick": create_todo});
+chrome.contextMenus.create({"title": "分享此页面到百度Hi", "onclick": share_to_hi});
 chrome.contextMenus.create({"title": "网页二维码", "onclick": create_qrc});
--- js/content-scripts.js
+++ js/content-scripts.js
@@ -5,11 +5,46 @@ chrome.extension.onMessage.addListener(function(message, sender, sendResponse) {
     if(message == 'createQRC'){
         var url = encodeURI(location.href);
         var qrcImgUrl = "http://qr.liantu.com/api.php?text=" + url;
-        var qrcImg = '<img id="script_qrc" src="' + qrcImgUrl + '" alt=""/>';
-        $("html").append(qrcImg);
+        var xIconImgURL = chrome.extension.getURL("img/x-icon.png");
+        var qrcImg = '<div id="script-qrc-div"><span id="script-qrc-xicon"><img src="' + xIconImgURL + '"/></span><img id="script-qrc-img" src="' + qrcImgUrl + '"/></div>';
+        $("html body").append(qrcImg);
         sendResponse('Create QRC OK.');
     }
-    else{
-        sendResponse('Create QRC fail.');
+    else if(message == 'getPageImg'){
+        var imgUrl = "http://st0.im.baidu.com/popup/resources/common/images/loading.gif";   // 默认图片
+        // 找到页面中的所有图片，并遍历
+        var allImg = $("body").find("img");
+        for(var i in allImg){
+            // 找出一个比较大的图
+            if(allImg[i].clientWidth >= 100 && allImg[i].clientHeight >= 20){
+                imgUrl = allImg[i].src;
+                break;
+            }
+        }
+        sendResponse(imgUrl);
     }
+    else if(message == 'getPageDes'){
+        var description = '';   // 默认描述
+        // 找到页面中的所有p标签，并遍历
+        var p = $("body").find("p");
+        var pc = 0; // 计数器
+        for(var i in p){
+            // 过滤文字比较少的段落
+            var text = $(p[i]).text();
+            if(text.length > 10){
+                description += text + "\n";
+                pc++;
+            }
+            if(pc >= 3){
+                break;
+            }
+        }
+        sendResponse(description);
+    }
+});
+
+$(document).on("click", "#script-qrc-xicon", function(){
+    $("#script-qrc-div")
+        .fadeOut()
+        .remove();
 });
